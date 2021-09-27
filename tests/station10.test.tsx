5940092d1549aeec730aa2759a48c210fcedda51
neco3coffee <neco3coffee@gmail.com>
2021-09-27 19:32:03 +0900

    hot

--- tests/station10.test.tsx
+++ tests/station10.test.tsx
@@ -5,27 +5,27 @@ import { createAsync } from './utils/createAsync'
 
 const breeds = Object.keys(nestedBreeds)
 
-// describe('<BreedsSelect />', () => {
-//   const fetch = jest.fn()
+describe('<BreedsSelect />', () => {
+  const fetch = jest.fn()
 
-//   window.fetch = fetch
-//   fetch.mockImplementation(fetchMock)
+  window.fetch = fetch
+  fetch.mockImplementation(fetchMock)
 
-//   it('exists', async () => {
-//     const { BreedsSelect } = require('../src/BreedsSelect')
-//     expect(BreedsSelect).toBeTruthy()
-//     await act(async () => {
-//       renderer.create(<BreedsSelect breeds={breeds} />)
-//     })
-//   })
+  // it('exists', async () => {
+  //   const { BreedsSelect } = require('../src/BreedsSelect')
+  //   expect(BreedsSelect).toBeTruthy()
+  //   await act(async () => {
+  //     renderer.create(<BreedsSelect breeds={breeds} />)
+  //   })
+  // })
 
-//   it('has `<select>` and `<option>` tags', async () => {
-//     const { BreedsSelect } = require('../src/BreedsSelect')
-//     const res = await createAsync(<BreedsSelect breeds={breeds} />)
-//     expect(res.root.findAllByType('select').length).not.toBe(0)
-//     expect(res.root.findAllByType('option').length).not.toBe(0)
-//   })
-// })
+  // it('has `<select>` and `<option>` tags', async () => {
+  //   const { BreedsSelect } = require('../src/BreedsSelect')
+  //   const res = await createAsync(<BreedsSelect breeds={breeds} />)
+  //   expect(res.root.findAllByType('select').length).not.toBe(0)
+  //   expect(res.root.findAllByType('option').length).not.toBe(0)
+  // })
+})
 
 describe('<App />', () => {
   it('value changes when `onChange` wes called', async () => {
